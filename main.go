7ab85c35c8ab97b975eff0f47f83469563247ff5
ataknz <atakanzzengin@gmail.com>
2021-05-08 19:24:53 +0300

    Change structure and implement qsort

--- main.go
+++ main.go
@@ -6,14 +6,23 @@ import (
 )
 
 func main() {
+	nums := []int{3, 5, 4, 1, 9, 105, 7, 6, 8, 10, 2}
+
 	// Recursion Sum
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	sumResult := recursion.RecursionSum(nums)
 
-	// Recursion count
+	// Recursion Count
 	items := []string{"a", "t", "a", "k", "a", "n"}
 	countResult := recursion.RecursionCount(items, 0)
 
+	// Recursion Max
+	maxResult := recursion.RecursionMax(nums)
+
+	// Recursion Quicksort
+	quicksortResult := recursion.Quicksort(nums)
+
 	fmt.Printf("Sum result: %v\n", sumResult)
 	fmt.Printf("Count result: %v\n", countResult)
+	fmt.Printf("Max result: %v\n", maxResult)
+	fmt.Printf("Quicksort result: %v\n", quicksortResult)
 }
--- recursion/recursion.go
+++ recursion/recursion.go
@@ -1,5 +1,10 @@
 package recursion
 
+import (
+	"math"
+	"math/rand"
+)
+
 func RecursionSum(nums []int) int {
 	if len(nums) == 0 {
 		return 0
@@ -14,4 +19,38 @@ func RecursionCount(items []string, count int) int {
 	}
 	count++
 	return count + RecursionCount(items[count:], count)
+}
+
+func RecursionMax(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	return int(math.Max(float64(nums[0]), float64(RecursionMax(nums[1:]))))
+}
+
+func Quicksort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+
+	left, right := 0, len(nums)-1
+
+	pivot := rand.Intn(len(nums))
+
+	nums[pivot], nums[right] = nums[right], nums[pivot]
+
+	for i, _ := range nums {
+		if nums[i] < nums[right] {
+			nums[left], nums[i] = nums[i], nums[left]
+			left++
+		}
+	}
+
+	nums[left], nums[right] = nums[right], nums[left]
+
+	Quicksort(nums[:left])
+	Quicksort(nums[left+1:])
+
+	return nums
 }
