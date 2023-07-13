b388d0bcb8acd40b0a8c9286cc05890ee20ddead
Atakan Zengin <atakanzzengin@gmail.com>
2023-07-13 22:10:19 +0200

    Add BubbleSort to sort package with its tests and benchmarks

--- sort/sort.go
+++ sort/sort.go
@@ -14,3 +14,44 @@ func InsertionSort(list []int) []int {
 
 	return list
 }
+
+func SelectionSort(list []int) []int {
+	for i := range list {
+		minIndex := i
+		for j := i; j < len(list); j++ {
+			if list[j] < list[minIndex] {
+				minIndex = j
+			}
+		}
+
+		temp := list[i]
+		list[i] = list[minIndex]
+		list[minIndex] = temp
+	}
+
+	return list
+}
+
+func BubbleSort(list []int) []int {
+	if len(list) == 1 {
+		return list
+	}
+
+	for i := 0; i < len(list); i++ {
+		swapped := false
+		for j := range list {
+			if j < len(list)-1 && list[j] > list[j+1] {
+				temp := list[j]
+				list[j] = list[j+1]
+				list[j+1] = temp
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			return list
+		}
+	}
+
+	return list
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -27,6 +27,11 @@ func TestInsertionSort(t *testing.T) {
 			input: []int{5, 2, 1, 6, 3, 4, 12, 9, 8, 11, 7, 10},
 			want:  []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
 		},
+		{
+			desc:  "sort 12 items with 3 duplicate",
+			input: []int{5, 2, 3, 6, 3, 4, 12, 3, 8, 11, 7, 10},
+			want:  []int{2, 3, 3, 3, 4, 5, 6, 7, 8, 10, 11, 12},
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
@@ -53,12 +58,148 @@ func BenchmarkInsertionSort(b *testing.B) {
 			desc:  "sort 12 items",
 			input: []int{5, 2, 1, 6, 3, 4, 12, 9, 8, 11, 7, 10},
 		},
+		{
+			desc:  "sort 12 items with 3 duplicate",
+			input: []int{5, 2, 3, 6, 3, 4, 12, 3, 8, 11, 7, 10},
+		},
 	}
 
 	for _, tC := range testCases {
 		b.Run(tC.desc, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				sort.InsertionSort(tC.input)
+			}
+		})
+	}
+}
+
+func TestSelectionSort(t *testing.T) {
+	testCases := []struct {
+		desc        string
+		input, want []int
+	}{
+		{
+			desc:  "sort 3 items",
+			input: []int{5, 2, 1},
+			want:  []int{1, 2, 5},
+		},
+		{
+			desc:  "sort 6 items",
+			input: []int{5, 2, 1, 6, 3, 4},
+			want:  []int{1, 2, 3, 4, 5, 6},
+		},
+		{
+			desc:  "sort 12 items",
+			input: []int{5, 2, 1, 6, 3, 4, 12, 9, 8, 11, 7, 10},
+			want:  []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+		},
+		{
+			desc:  "sort 12 items with 3 duplicate",
+			input: []int{5, 2, 3, 6, 3, 4, 12, 3, 8, 11, 7, 10},
+			want:  []int{2, 3, 3, 3, 4, 5, 6, 7, 8, 10, 11, 12},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			actual := sort.SelectionSort(tC.input)
+			assert.Equal(t, tC.want, actual)
+		})
+	}
+}
+
+func BenchmarkSelectionSort(b *testing.B) {
+	testCases := []struct {
+		desc  string
+		input []int
+	}{
+		{
+			desc:  "sort 3 items",
+			input: []int{5, 2, 1},
+		},
+		{
+			desc:  "sort 6 items",
+			input: []int{5, 2, 1, 6, 3, 4},
+		},
+		{
+			desc:  "sort 12 items",
+			input: []int{5, 2, 1, 6, 3, 4, 12, 9, 8, 11, 7, 10},
+		},
+		{
+			desc:  "sort 12 items with 3 duplicate",
+			input: []int{5, 2, 3, 6, 3, 4, 12, 3, 8, 11, 7, 10},
+		},
+	}
+
+	for _, tC := range testCases {
+		b.Run(tC.desc, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sort.SelectionSort(tC.input)
+			}
+		})
+	}
+}
+
+func TestBubbleSort(t *testing.T) {
+	testCases := []struct {
+		desc        string
+		input, want []int
+	}{
+		{
+			desc:  "sort 3 items",
+			input: []int{5, 2, 1},
+			want:  []int{1, 2, 5},
+		},
+		{
+			desc:  "sort 6 items",
+			input: []int{5, 2, 1, 6, 3, 4},
+			want:  []int{1, 2, 3, 4, 5, 6},
+		},
+		{
+			desc:  "sort 12 items",
+			input: []int{5, 2, 1, 6, 3, 4, 12, 9, 8, 11, 7, 10},
+			want:  []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+		},
+		{
+			desc:  "sort 12 items with 3 duplicate",
+			input: []int{5, 2, 3, 6, 3, 4, 12, 3, 8, 11, 7, 10},
+			want:  []int{2, 3, 3, 3, 4, 5, 6, 7, 8, 10, 11, 12},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			actual := sort.BubbleSort(tC.input)
+			assert.Equal(t, tC.want, actual)
+		})
+	}
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	testCases := []struct {
+		desc  string
+		input []int
+	}{
+		{
+			desc:  "sort 3 items",
+			input: []int{5, 2, 1},
+		},
+		{
+			desc:  "sort 6 items",
+			input: []int{5, 2, 1, 6, 3, 4},
+		},
+		{
+			desc:  "sort 12 items",
+			input: []int{5, 2, 1, 6, 3, 4, 12, 9, 8, 11, 7, 10},
+		},
+		{
+			desc:  "sort 12 items with 3 duplicate",
+			input: []int{5, 2, 3, 6, 3, 4, 12, 3, 8, 11, 7, 10},
+		},
+	}
+
+	for _, tC := range testCases {
+		b.Run(tC.desc, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sort.BubbleSort(tC.input)
 			}
 		})
 	}
